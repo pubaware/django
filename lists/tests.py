e7a4d73c1151f1e641e5406f45bef0a6be591c64
diram <diram.wok@protonmail.com>
2020-04-20 22:50:14 +0800

    Add functional test to test add 2nd item to the list, failed before refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,14 @@ from lists.views import home_page
 
 # Create your tests here.
 class HomePageTest(TestCase):
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
