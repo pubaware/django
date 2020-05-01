519986bc0433acfcd02a2a56c2a4a17c55d31c1c
diram <diram.wok@protonmail.com>
2020-05-02 00:29:51 +0800

    new URL, view and template to dispaly lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -99,7 +99,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         # She notices that her list has a unique URL
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/list/.+')
+        self.assertRegex(edith_list_url, '/lists/.+')
    
         # Now a new user, Francis, comes along to the site.o
 
@@ -123,14 +123,14 @@ class NewVisitorTest(LiveServerTestCase):
         Francis starts a new list by entering a new item. He
         is less interesting than Edith...
         '''
-        inputbox = self.browser.find_element_by_id('id new item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, '/list/.+')
+        self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again, there is no trace of Edith's list
--- lists/tests.py
+++ lists/tests.py
@@ -6,8 +6,23 @@ from lists.models import Item
 from lists.views import home_page
 
 # Create your tests here.
-class ItemModelTest(TestCase):
+class ListViewTest(TestCase):
 
+    def test_uses_list_tempalte(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+class ItemModelTest(TestCase):
+    '''
     def test_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
@@ -16,7 +31,7 @@ class ItemModelTest(TestCase):
 
         self.assertIn('itemey 1', response.content.decode())
         self.assertIn('itemey 2', response.content.decode())
-
+    '''
     def test_saving_and_retrieving_items(self):
         first_item = Item()
         first_item.text = 'The first (ever) list item'
@@ -54,12 +69,13 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+    '''
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-
+    '''
+    """
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         '''
@@ -80,4 +96,5 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
         '''
         self.assertTemplateUsed(response, 'home.html')
+    """
         
